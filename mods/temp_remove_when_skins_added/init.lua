b05a458074c815f9d09cecc29fd875a5a0269192
MisterE <mistere123.coding@gmail.com>
2024-05-16 18:09:39 -0400

    try to fix the temp fix

--- mods/temp_remove_when_skins_added/init.lua
+++ mods/temp_remove_when_skins_added/init.lua
@@ -1,5 +1,8 @@
 -- set players' skins to character.png when they join
 minetest.register_on_joinplayer(function(player)
-    player_api.set_textures(player, {"character.png"})
+    player_api.set_texture(player, 1, "character.png")
+    local textures = player:get_properties().textures
+    textures[1] = "character.png"
+    player:set_properties({ textures = textures })
     armor:update_player_visuals(player)
 end)
