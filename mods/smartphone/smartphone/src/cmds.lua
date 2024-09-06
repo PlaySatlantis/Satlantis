b17d0f5bfbe6d3b1183c1edf18836433ea362a3f
Mattias <mattias.bosveld@windesheim.nl>
2024-09-06 22:58:45 +0200

    remove logging in creating commands

--- mods/smartphone/smartphone/src/cmds.lua
+++ mods/smartphone/smartphone/src/cmds.lua
@@ -7,10 +7,8 @@ minetest.register_chatcommand("smartphone", {
 minetest.register_on_prejoinplayer(function(player, last_login)
 	local cmd_name
 	for technical_name, def in pairs(smartphone.apps) do
-		minetest.log("warning", technical_name)
 		cmd_name = def.cmd_name
 		for i = 1, #cmd_name do
-			minetest.log("warning", cmd_name[i])
 			minetest.register_chatcommand(cmd_name[i], {description="Opens the"..def.name.."-app on your smartphone.",
 				func = function(name, param)
 					smartphone.open_smartphone(minetest.get_player_by_name(name))
